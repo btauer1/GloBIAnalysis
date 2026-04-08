a69cc88f10cbc6ed6cdcacc9a0c78498752c5464
NelleFeliciano <nfeliciano@smith.edu>
2026-04-08 11:52:22 -0400

    visualizing locations of interactions map function progress

---
+++ R/visInteractMap.R
@@ -0,0 +1,25 @@
+#' Interactive map detailing locations of bee-plant interactions
+#'
+#' @description Add brief description to function(s)
+#' @details Specifics about functionality, inputs, etc.
+#' @param input Brief description of input. (Add as many params as needed)
+#' @return Brief description of what function returns/prints.
+#' @export Added here to export function to user (text not needed here)
+#' @import package Import package used to supplement function. (Add as many as needed)
+#' @examples
+#' Put function call as example.
+#'
+
+visInteractMap <- function() {
+  library(leaflet)
+
+  places <- GloBI_Curated_sample |>
+    select(x, y)
+
+  # create map with points
+  leaflet(places) |>
+    addTiles() |>  # Adds OpenStreetMap background
+    addMarkers(lng = ~x, lat = ~y)
+}
+
+# NOTES: Could add popups about information on the specific interactions in each place
