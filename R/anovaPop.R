53a171865467f9ef1b4f0d210405063e06393069
mkgramdinger <mkgramdinger@gmail.com>
2026-04-22 11:35:43 -0400

    updated functions

--- R/anovaPop.R
+++ R/anovaPop.R
@@ -1,35 +1,51 @@
 #' ANOVA analysis of bee and plant population dynamics
 #'
-#' @description Function that returns [INSERT SUMMARY STATISTICS]
-#' @details Specifics about functionality, inputs, etc.
-#' @param input Brief description of input. (Add as many params as needed)
-#' @return Brief description of what function returns/prints.
+#' @description Function that returns if the relationship between interaction type and bee/plant endemicity is significant.
+#' @details Runs two-way ANVOA test with endemicity as the independent variable and interaction type as the dependent variable.
+#' @param input group: bee or plant, which type of endemicity is being tested
+#' @param input level: significance level of ANOVA regression
+#' @return Prints message saying if ANOVA is significant or not.
 #' @export
-#' @import dplyr
+#' @import stats
 #' @examples
-#' Put function call as example.
+#' anovaPop(bee, 0.05)
+#' anovaPop(plant, 0.1)
 #'
 
 data <- GloBI_Curated_sample
 
 #group is plant_endemic or bee_endemic
-anovaPop <- function(group){
+anovaPop <- function(group, level){
+
+  #check group is valid
+  group <- as.character(group)
+  if (!group %in% c("plant", "bee")){
+    stop("Invalid group argument. Valid groups are \"plant\" or \"bee\".")
+  }
+
+  #check level is valid
+  if (is.double(level) != T){
+    stop("Invalid argument. Level must be a numeric value.")
+  } else if (level < 0 | level > 1) {
+    warning("Level should be between 0 and 1. Common levels are 0.1, 0.05, 0.01, 0.001.")
+  }
 
   #choose endemic type
   if (group == "plant"){
-    var = plant_endemic
+    var <- data$plant_endemic
   }
   else if (group == "bee"){
-    var = bee_endemic
-  }
-  else {
-    stop("Valid arguments are \"plant\" or \"bee\".")
+    var <- data$bee_endemic
   }
 
   #run ANVOA test
-  anova <- summary(aov(var ~ interactionTypeName, data = data))
+  anova_table <- summary(aov(var ~ interactionTypeName, data = data))
 
   #significance of ANVOA
-  if
+  if (anova_table[[1]]$`Pr(>F)`[1] <= level){
+    cat("This interaction IS statistically significant (p =", anova_table[[1]]$`Pr(>F)`[1], ", df =", anova_table[[1]]$Df[1], ").")
+  } else {
+    cat("This interaction IS NOT statistically significant (p =", anova_table[[1]]$`Pr(>F)`[1], ", df =", anova_table[[1]]$Df[1], ").")
+  }
 }
 
