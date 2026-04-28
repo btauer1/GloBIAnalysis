593ed209bd58a09fd8d4f99df355e85e2c1a8e27
btauer1 <btauer@smith.edu>
2026-04-27 23:51:42 -0400

    test development

--- tests/testthat/test-visInteractMap.R
+++ tests/testthat/test-visInteractMap.R
@@ -1,5 +1,8 @@
 test_that("visInteractMap", {
   expect_snapshot(cat(visInteractMap()))
-  expect_snapshot(cat(visInteractMap(variable = "bee_genus", filter = "Lasioglossum")))})
+  expect_snapshot(cat(visInteractMap(variable = "bee_genus", filter = "Lasioglossum")))
+  expect_error(visInteractMap(variable = "bee_genus"))
+  expect_error(visInteractMap(variable = "bee_genus", filter = 7))
+  })
 
 #interesting error happening visInteractMap is in namespace, unclear why testthat cannot find it
