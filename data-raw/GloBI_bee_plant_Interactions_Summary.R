609fa36e0a92c44acd141bfd403bdf6ce2cbbaf4
btauer1 <btauer@smith.edu>
2026-05-04 11:55:23 -0400

    updated variable calling

--- data-raw/GloBI_bee_plant_Interactions_Summary.R
+++
@@ -1,6 +0,0 @@
-## code to prepare `GloBI_bee_plant_Interactions_Summary` dataset goes here
-
-library(readr)
-GloBI_bee_plant_Interactions_Summary <- read_csv("data-raw/GloBI_bee_plant_Interactions_Summary.csv")
-
-usethis::use_data(GloBI_bee_plant_Interactions_Summary, overwrite = TRUE)
--- tests/testthat/test-visInteractMap.R
+++
@@ -1,8 +0,0 @@
-test_that("visInteractMap", {
-  expect_snapshot(cat(visInteractMap()))
-  expect_snapshot(cat(visInteractMap(variable = "bee_genus", filter = "Lasioglossum")))
-  expect_error(visInteractMap(variable = "bee_genus"))
-  expect_error(visInteractMap(variable = "bee_genus", filter = 7))
-  })
-
-#interesting error happening visInteractMap is in namespace, unclear why testthat cannot find it
